b61b9d1ff0e1572561cfaff448d7aec7ea547386
AresConnor <39639716+AresConnor@users.noreply.github.com>
2023-03-27 21:07:35 +0800

    java搜索跨平台,下载界面改进

--- MCSL2_Utils.py
+++ MCSL2_Utils.py
@@ -1,37 +1,26 @@
-from json import loads
-from os import mkdir
+import json
+from typing import Callable, Any
+from os import  mkdir
 from os import path as ospath
 
+from PyQt5 import QtCore
+from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QDialog
 from requests import get
 
-from MCSL2_AskDialog import *  # noqa: F403
-from MCSL2_Dialog import *  # noqa: F403
+from MCSL2_AskDialog import Ui_MCSL2_AskDialog
+from MCSL2_Dialog import Ui_MCSL2_Dialog
 
 
-# Customize dialogs
-class MCSL2Dialog(QDialog, Ui_MCSL2_Dialog):
-    def __init__(self, Tip):
-        super(MCSL2Dialog, self).__init__()
-        self.setupUi(self)
-        self.Dialog_label.setText(Tip)
+def singleton(cls):
+    instances = {}
 
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
 
-class MCSL2AskDialog(QDialog, Ui_MCSL2_AskDialog):
-    def __init__(self, Tip):
-        super(MCSL2AskDialog, self).__init__()
-        self.setupUi(self)
-        self.Dialog_label.setText(Tip)
-
-
-# The function of calling MCSL2 Dialog
-def CallMCSL2Dialog(Tip, isNeededTwoButtons):
-    if isNeededTwoButtons == 0:
-        MCSL2Dialog(Tip).exec()
-    elif isNeededTwoButtons == 1:
-        MCSL2AskDialog(Tip).exec()
-    else:
-        pass
+    return get_instance
 
 
 def InitMCSL():
@@ -82,6 +71,27 @@ def ParseDownloaderAPIUrl(DownloadSource, DownloadType):
     return SubWidgetNames, DownloadUrls, FileNames, FileFormats
 
 
+def ParseDownloaderAPIUrl1(DownloadSource):
+    UrlPrefix = "https://raw.iqiq.io/LxHTT/MCSLDownloaderAPI/master/"
+    SourceSuffix = ["SharePoint", "Gitee", "luoxisCloud", "GHProxy", "GitHub"]
+    TypeSuffix = [
+        "/JavaDownloadInfo.json",
+        "/SpigotDownloadInfo.json",
+        "/PaperDownloadInfo.json",
+        "/BungeeCordDownloadInfo.json",
+        "/OfficialCoreDownloadInfo.json",
+    ]
+    rv = {}
+    for i in range(len(TypeSuffix)):
+        DownloadAPIUrl = UrlPrefix + SourceSuffix[DownloadSource] + TypeSuffix[i]
+        SubWidgetNames, DownloadUrls, FileNames, FileFormats = DecodeDownloadJsons(DownloadAPIUrl)
+        rv.update({
+            i: dict(zip(("SubWidgetNames", "DownloadUrls", "FileNames", "FileFormats"),
+                        (SubWidgetNames, DownloadUrls, FileNames, FileFormats)))
+        })
+    return {DownloadSource: rv}
+
+
 def DecodeDownloadJsons(RefreshUrl):
     SubWidgetNames = []
     DownloadUrls = []
@@ -90,11 +100,11 @@ def DecodeDownloadJsons(RefreshUrl):
     try:
         DownloadJson = get(RefreshUrl).text
     except:
-        Tip = "无法连接MCSLAPI，\n\n请检查网络或系统代理设置"
-        CallMCSL2Dialog(Tip, isNeededTwoButtons=0)
-        return -1, -1, -1, -1
+        # Tip = "无法连接MCSLAPI，\n\n请检查网络或系统代理设置"
+        # CallMCSL2Dialog(Tip, isNeededTwoButtons=0)
+        return -2, -2, -2, -2
     try:
-        PyDownloadList = loads(DownloadJson)["MCSLDownloadList"]
+        PyDownloadList = json.loads(DownloadJson)["MCSLDownloadList"]
         for i in PyDownloadList:
             SubWidgetName = i["name"]
             SubWidgetNames.insert(0, SubWidgetName)
@@ -107,15 +117,161 @@ def DecodeDownloadJsons(RefreshUrl):
         return SubWidgetNames, DownloadUrls, FileNames, FileFormats
     except:
         print(DownloadJson)
-        Tip = "可能解析api内容失败\n\n请检查网络或自己的节点设置"
-        CallMCSL2Dialog(Tip, isNeededTwoButtons=0)
+        # Tip = "可能解析api内容失败\n\n请检查网络或自己的节点设置"
+        # CallMCSL2Dialog(Tip, isNeededTwoButtons=0)
         return -1, -1, -1, -1
 
 
-# def GetFileVersion(File):
-#     FileVersionInfo = GetFileVersionInfo(File, sep)
-#     FileVersionMS = FileVersionInfo['FileVersionMS']
-#     FileVersionLS = FileVersionInfo['FileVersionLS']
-#     Version = '%d.%d.%d.%04d' % (
-#         HIWORD(FileVersionMS), LOWORD(FileVersionMS), HIWORD(FileVersionLS), LOWORD(FileVersionLS))
-#     return Version
+def readJsonFile(filename):
+    filename = 'MCSL2/' + filename
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+
+def saveJsonFile(filename, data):
+    filename = 'MCSL2/' + filename
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True)
+
+
+# Customize dialogs
+class MCSL2Dialog(QDialog, Ui_MCSL2_Dialog):
+    def __init__(self, Tip, parent=None):
+        super(MCSL2Dialog, self).__init__(parent=parent)
+        self.setupUi(self)
+        self.Dialog_label.setText(Tip)
+
+
+class MCSL2AskDialog(QDialog, Ui_MCSL2_AskDialog):
+    def __init__(self, Tip, parent=None):
+        super(MCSL2AskDialog, self).__init__(parent=parent)
+        self.setupUi(self)
+        self.Dialog_label.setText(Tip)
+
+
+# The function of calling MCSL2 Dialog
+def CallMCSL2Dialog(Tip, isNeededTwoButtons, parent=None):
+    if isNeededTwoButtons == 0:
+        dialog = MCSL2Dialog(Tip, parent)
+    elif isNeededTwoButtons == 1:
+        dialog = MCSL2AskDialog(Tip, parent)
+    else:
+        return
+    dialog.exec_()
+    return dialog
+
+
+class JsonReadThread(QThread):
+    readSignal = pyqtSignal(dict)
+
+    def __init__(self, filename, finishSlot: Callable[[dict, ], Any] = ...):
+        super().__init__()
+        self.filename = filename
+        self.data = None
+        if finishSlot is not ...:
+            self.readSignal.connect(finishSlot)
+
+    def getFileName(self):
+        return self.filename
+
+    def run(self):
+        self.data = readJsonFile(self.filename)
+        self.readSignal.emit(self.data)
+
+    def getData(self):
+        return self.data
+
+
+class JsonSaveThread(QThread):
+    saveSignal = pyqtSignal(bool)
+
+    def __init__(self, filename, data, finishSlot: Callable[[], Any] = ...):
+        super().__init__()
+        self.filename = filename
+        self.data = data
+        if finishSlot is not ...:
+            self.saveSignal.connect(finishSlot)
+
+    def getFileName(self):
+        return self.filename
+
+    def run(self):
+        try:
+            saveJsonFile(self.filename, self.data)
+            self.saveSignal.emit(True)
+        except Exception as e:
+            print(e)
+            self.saveSignal.emit(False)
+
+
+@singleton
+class JsonReadThreadFactory:
+    """
+        虽然我不会去实例化这个类，但我还是加了个singleton装饰器...
+    """
+
+    @classmethod
+    def create(cls, filename, finishSlot: Callable[[dict, ], Any] = ...) -> JsonReadThread:
+        return JsonReadThread(filename, finishSlot)
+
+
+@singleton
+class JsonSaveThreadFactory:
+    """
+        虽然我不会去实例化这个类，但我还是加了个singleton装饰器...
+    """
+
+    @classmethod
+    def create(cls, filename, data, finishSlot: Callable[[], Any] = ...) -> JsonSaveThread:
+        return JsonSaveThread(filename, data, finishSlot)
+
+
+class FetchDownloadURLThread(QThread):
+    """
+    用于获取网页内容的线程
+    结束时发射fetchSignal信号，参数为url和data组成的元组
+    """
+    fetchSignal = pyqtSignal(dict)
+
+    def __init__(self, downloadSrc, finishSlot: Callable = ...):
+        super().__init__()
+        self._id = None
+        self.downloadSrc = downloadSrc
+        self.data = None
+        if finishSlot is not ...:
+            self.fetchSignal.connect(finishSlot)
+
+    def getURL(self):
+        return self.url
+
+    def run(self):
+        self.fetchSignal.emit(ParseDownloaderAPIUrl1(self.downloadSrc))
+
+    def getData(self):
+        return self.data
+
+
+@singleton
+class FetchDownloadURLThreadFactory:
+    singletonThread: dict[int, FetchDownloadURLThread] = {}
+
+    @classmethod
+    def create(cls,
+               downloadSrc: int,
+               _singleton=False,
+               finishSlot=...) -> FetchDownloadURLThread:
+
+        print({k: v.isRunning() for k, v in cls.singletonThread.items()})
+        if _singleton:
+
+            if downloadSrc in cls.singletonThread and cls.singletonThread[downloadSrc].isRunning():
+                print("线程已存在，返回已存在的线程")
+
+                return cls.singletonThread[downloadSrc]
+            else:
+                thread = FetchDownloadURLThread(downloadSrc, finishSlot)
+                cls.singletonThread[downloadSrc] = thread
+                return thread
+        else:
+            return FetchDownloadURLThread(downloadSrc, finishSlot)
