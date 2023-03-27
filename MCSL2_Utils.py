ada32ab7d1938283d58c37c7fce84d1ec95c33d2
AresConnor <39639716+AresConnor@users.noreply.github.com>
2023-03-27 19:01:53 +0800

    before merge

--- MCSL2_Utils.py
+++ MCSL2_Utils.py
@@ -1,66 +1,25 @@
-from json import loads
-from os import mkdir
-from os import path as ospath
+import json
+import time
+from typing import Optional, Callable, Union, Iterable, Any
+import weakref
 
+from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QDialog
 from requests import get
 
-from MCSL2_AskDialog import *  # noqa: F403
-from MCSL2_Dialog import *  # noqa: F403
+from MCSL2_AskDialog import Ui_MCSL2_AskDialog
+from MCSL2_Dialog import Ui_MCSL2_Dialog
 
 
-# Customize dialogs
-class MCSL2Dialog(QDialog, Ui_MCSL2_Dialog):
-    def __init__(self, Tip):
-        super(MCSL2Dialog, self).__init__()
-        self.setupUi(self)
-        self.Dialog_label.setText(Tip)
+def singleton(cls):
+    instances = {}
 
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
 
-class MCSL2AskDialog(QDialog, Ui_MCSL2_AskDialog):
-    def __init__(self, Tip):
-        super(MCSL2AskDialog, self).__init__()
-        self.setupUi(self)
-        self.Dialog_label.setText(Tip)
-
-
-# The function of calling MCSL2 Dialog
-def CallMCSL2Dialog(Tip, isNeededTwoButtons):
-    if isNeededTwoButtons == 0:
-        MCSL2Dialog(Tip).exec()
-    elif isNeededTwoButtons == 1:
-        MCSL2AskDialog(Tip).exec()
-    else:
-        pass
-
-
-def InitMCSL():
-    if not ospath.exists(r"MCSL2"):
-        mkdir(r"MCSL2")
-        CallMCSL2Dialog(Tip="请注意：\n\n本程序无法在125%的\n\nDPI缩放比下正常运行。\n(本提示仅在首次启动出现)",
-                        isNeededTwoButtons=0)
-        mkdir(r"MCSL2/Aria2")
-        with open(r"./MCSL2/MCSL2_Config.json", "w+", encoding="utf-8") as InitConfig:
-            ConfigTemplate = ""
-            InitConfig.write(ConfigTemplate)
-            InitConfig.close()
-        with open(
-                r"./MCSL2/MCSL2_ServerList.json", "w+", encoding="utf-8"
-        ) as InitServerList:
-            ServerListTemplate = '{\n  "MCSLServerList": [\n    {\n      "name": "MCSLReplacer",\n      ' \
-                                 '"core_file_name": "MCSLReplacer",\n      "java_path": "MCSLReplacer",' \
-                                 '\n      "min_memory": "MCSLReplacer",\n      "max_memory": "MCSLReplacer"\n    }\n  ' \
-                                 ']\n} '
-            InitServerList.write(ServerListTemplate)
-            InitServerList.close()
-        if not ospath.exists(r"Servers"):
-            mkdir(r"./Servers")
-
-        pass
-    else:
-        if not ospath.exists(r"Servers"):
-            mkdir(r"./Servers")
-        pass
+    return get_instance
 
 
 def ParseDownloaderAPIUrl(DownloadSource, DownloadType):
@@ -94,7 +53,7 @@ def DecodeDownloadJsons(RefreshUrl):
         CallMCSL2Dialog(Tip, isNeededTwoButtons=0)
         return -1, -1, -1, -1
     try:
-        PyDownloadList = loads(DownloadJson)["MCSLDownloadList"]
+        PyDownloadList = json.loads(DownloadJson)["MCSLDownloadList"]
         for i in PyDownloadList:
             SubWidgetName = i["name"]
             SubWidgetNames.insert(0, SubWidgetName)
@@ -112,10 +71,157 @@ def DecodeDownloadJsons(RefreshUrl):
         return -1, -1, -1, -1
 
 
-# def GetFileVersion(File):
-#     FileVersionInfo = GetFileVersionInfo(File, sep)
-#     FileVersionMS = FileVersionInfo['FileVersionMS']
-#     FileVersionLS = FileVersionInfo['FileVersionLS']
-#     Version = '%d.%d.%d.%04d' % (
-#         HIWORD(FileVersionMS), LOWORD(FileVersionMS), HIWORD(FileVersionLS), LOWORD(FileVersionLS))
-#     return Version
+def readJsonFile(filename):
+    filename = 'MCSL2/' + filename
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+
+def saveJsonFile(filename, data):
+    filename = 'MCSL2/' + filename
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True)
+
+
+# Customize dialogs
+class MCSL2Dialog(QDialog, Ui_MCSL2_Dialog):
+    def __init__(self, Tip, parent=None):
+        super(MCSL2Dialog, self).__init__(parent=parent)
+        self.setupUi(self)
+        self.Dialog_label.setText(Tip)
+
+
+class MCSL2AskDialog(QDialog, Ui_MCSL2_AskDialog):
+    def __init__(self, Tip, parent=None):
+        super(MCSL2AskDialog, self).__init__(parent=parent)
+        self.setupUi(self)
+        self.Dialog_label.setText(Tip)
+
+
+# The function of calling MCSL2 Dialog
+def CallMCSL2Dialog(Tip, isNeededTwoButtons, parent=None):
+    if isNeededTwoButtons == 0:
+        dialog = MCSL2Dialog(Tip, parent)
+    elif isNeededTwoButtons == 1:
+        dialog = MCSL2AskDialog(Tip, parent)
+    else:
+        return
+    dialog.exec()
+    return dialog
+
+
+class JsonReadThread(QThread):
+    readSignal = pyqtSignal(dict)
+
+    def __init__(self, filename, finishSlot: Callable[[dict, ], Any] = ...):
+        super().__init__()
+        self.filename = filename
+        self.data = None
+        if finishSlot is not ...:
+            self.readSignal.connect(finishSlot)
+
+    def getFileName(self):
+        return self.filename
+
+    def run(self):
+        self.data = readJsonFile(self.filename)
+        self.readSignal.emit(self.data)
+
+    def getData(self):
+        return self.data
+
+
+class JsonSaveThread(QThread):
+    saveSignal = pyqtSignal(bool)
+
+    def __init__(self, filename, data, finishSlot: Callable[[], Any] = ...):
+        super().__init__()
+        self.filename = filename
+        self.data = data
+        if finishSlot is not ...:
+            self.saveSignal.connect(finishSlot)
+
+    def getFileName(self):
+        return self.filename
+
+    def run(self):
+        try:
+            saveJsonFile(self.filename, self.data)
+            self.saveSignal.emit(True)
+        except Exception as e:
+            print(e)
+            self.saveSignal.emit(False)
+
+
+@singleton
+class JsonReadThreadFactory:
+    """
+        虽然我不会去实例化这个类，但我还是加了个singleton装饰器...
+    """
+
+    @classmethod
+    def create(cls, filename, finishSlot: Callable[[dict, ], Any] = ...) -> JsonReadThread:
+        return JsonReadThread(filename, finishSlot)
+
+
+@singleton
+class JsonSaveThreadFactory:
+    """
+        虽然我不会去实例化这个类，但我还是加了个singleton装饰器...
+    """
+
+    @classmethod
+    def create(cls, filename, data, finishSlot: Callable[[], Any] = ...) -> JsonSaveThread:
+        return JsonSaveThread(filename, data, finishSlot)
+
+
+class FetchDownloadURLThread(QThread):
+    """
+    用于获取网页内容的线程
+    结束时发射fetchSignal信号，参数为url和data组成的元组
+    """
+    fetchSignal = pyqtSignal(tuple)
+
+    def __init__(self, arg, finishSlot: Callable = ...):
+        super().__init__()
+        self._id = None
+        self.arg = arg
+        self.data = None
+        if finishSlot is not ...:
+            self.fetchSignal.connect(finishSlot)
+
+    def getURL(self):
+        return self.url
+
+    def run(self):
+        arg1, arg2 = self.arg
+        r = ParseDownloaderAPIUrl(arg1, arg2)
+        time.sleep(3)
+        self.fetchSignal.emit(r)
+
+    def getData(self):
+        return self.data
+
+
+@singleton
+class FetchDownloadURLThreadFactory:
+    singletonThread = {}
+
+    @classmethod
+    def create(cls, arg: tuple, _singleton=False,
+               finishSlot=...) -> FetchDownloadURLThread:
+        print({k: v.isRunning() for k, v in cls.singletonThread.items()})
+        if _singleton:
+
+            if arg in cls.singletonThread and cls.singletonThread[arg].isRunning():
+                print("线程已存在，返回已存在的线程")
+
+                return cls.singletonThread[arg]
+            else:
+                thread = FetchDownloadURLThread(arg, finishSlot)
+                # 添加弱引用
+                cls.singletonThread[arg] = thread
+                return thread
+        else:
+            return FetchDownloadURLThread(arg, finishSlot)
