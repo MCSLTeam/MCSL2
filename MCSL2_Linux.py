e7115a87f719b94f2b7994cc4b366ccce05a2070
LxHTT <lxhtz.dl@qq.com>
2023-04-16 10:16:42 +0800

    使Linux版本不设置环境变量即可直接运行

--- MCSL2_Linux.py
+++ MCSL2_Linux.py
@@ -1,7 +1,5 @@
-import platform
-from json import dumps, loads
-from os import getcwd, environ, remove
-from shutil import copy
+from json import dump
+from os import getcwd, environ, remove, path as ospath
 from subprocess import CalledProcessError
 from sys import argv, exit
 
@@ -13,15 +11,16 @@ from PyQt5.QtWidgets import (
     QGraphicsDropShadowEffect,
     QMainWindow,
 )
-from requests import get
 
 from MCSL2_Libs import MCSL2_Icon as _  # noqa: F401
 from MCSL2_Libs import MCSL2_JavaDetector
+from MCSL2_Libs.MCSL2_Dialog import CallMCSL2Dialog
 from MCSL2_Libs.MCSL2_DownloadURLParser import FetchDownloadURLThreadFactory
+from MCSL2_Libs.MCSL2_Init import InitMCSL
 from MCSL2_Libs.MCSL2_JavaDetector import GetJavaVersion, Java
 from MCSL2_Libs.MCSL2_MainWindow import *  # noqa: F403
-from MCSL2_Libs.MCSL2_Utils import *
-
+from MCSL2_Libs.MCSL2_ServerController import ServerSaver
+from MCSL2_Libs.MCSL2_Updater import Updater
 
 # Initialize MainWindow
 class MCSL2MainWindow(QMainWindow, Ui_MCSL2_MainWindow):
@@ -69,7 +68,7 @@ class MCSL2MainWindow(QMainWindow, Ui_MCSL2_MainWindow):
 
         # Functions binding
         self.DownloadSwitcher_TabWidget.currentChanged.connect(self.RefreshDownloadType)
-        self.Start_PushButton.clicked.connect(self.LaunchMinecraftServer)
+        # self.Start_PushButton.clicked.connect(self.LaunchMinecraftServer)
         self.Manual_Import_Core_PushButton.clicked.connect(self.ManuallyImportCore)
         self.Download_Java_PushButton.clicked.connect(self.ToDownloadJava)
         self.Check_Update_PushButton.clicked.connect(self.CheckUpdate)
@@ -364,187 +363,25 @@ class MCSL2MainWindow(QMainWindow, Ui_MCSL2_MainWindow):
         ChkVal.append(NameStatus)
         ChkVal.append(JavaStatus)
         ChkVal.append(CoreStatus)
-        if (ChkVal[0] == 1):
-            if (ChkVal[1] == 1):
-                if (ChkVal[2] == 1):
-                    if (ChkVal[3] == 1):
-                        if (ChkVal[4] == 1):
-                            CanCreate = 1
-                            Tip = "关闭此窗口后，\n\n服务器将会开始部署。"
-                        else:
-                            CanCreate = 0
-                            Tip = "只剩服务器核心没设置好力\n\n（喜"
-                    else:
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩Java没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "只剩Java和服务器核心没设置好力\n\n（喜"
-                else:
-                    if (ChkVal[3] == 1):
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩服务器名称没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "只剩服务器名称和服务器核心没设置好力\n\n（喜"
-                    else:
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩服务器名称和Java没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "你只设置好了内存\n\n（恼"
-            else:
-                if (ChkVal[2] == 1):
-                    if (ChkVal[3] == 1):
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩最大内存没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "只剩最大内存和服务器核心没设置好力\n\n（喜"
-                    else:
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩最大内存和Java没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "服务器核心、Java和最大内存还没设置好呢\n\n（恼"
-                else:
-                    if (ChkVal[3] == 1):
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩服务器名称和最大内存没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "服务器核心、服务器名称和最大内存还没设置好呢\n\n（恼"
-                    else:
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "Java、服务器名称和最大内存还没设置好呢\n\n（恼"
-                        else:
-                            CanCreate = 0
-                            Tip = "你只设置好了最小内存\n\n（恼"
-        else:
-            if (ChkVal[1] == 1):
-                if (ChkVal[2] == 1):
-                    if (ChkVal[3] == 1):
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩最小内存没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "只剩服务器核心和最小内存没设置好力\n\n（喜"
-                    else:
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩Java和最小内存没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "服务器核心、Java和最小内存还没设置好呢\n\n（恼"
-                else:
-                    if (ChkVal[3] == 1):
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩服务器名称和最小内存没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "服务器核心、服务器名称和最小内存还没设置好呢\n\n（恼"
-                    else:
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "Java、服务器名称和最小内存还没设置好呢\n\n（恼"
-                        else:
-                            CanCreate = 0
-                            Tip = "你只设置好了最大内存\n\n（恼"
-            else:
-                if (ChkVal[2] == 1):
-                    if (ChkVal[3] == 1):
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "只剩内存没设置好力\n\n（喜"
-                        else:
-                            CanCreate = 0
-                            Tip = "服务器核心和内存还没设置好呢\n\n（恼"
-                    else:
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "Java和内存还没设置好呢\n\n（恼"
-                        else:
-                            CanCreate = 0
-                            Tip = "你只设置好了服务器名称\n\n（恼"
-                else:
-                    if (ChkVal[3] == 1):
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "服务器名称和内存还没设置好呢\n\n（恼"
-                        else:
-                            CanCreate = 0
-                            Tip = "你只设置好了Java\n\n（恼"
-                    else:
-                        if (ChkVal[4] == 1):
-                            CanCreate = 0
-                            Tip = "你只设置好了服务器核心\n\n（恼"
-                        else:
-                            CanCreate = 0
-                            Tip = "你什么都没设置好呢\n\n（恼"
-                            # 终于写完了.jpg
+
+        CheckAvailable = ServerSaver.CheckAvailableSaveServer(ChkVal)
+        CanCreate = CheckAvailable[0]
+        Tip = CheckAvailable[1]
 
         # Server processor
         if CanCreate == 0:
             CallMCSL2Dialog(Tip, 0)
         elif CanCreate == 1:
-            global GlobalServerList
-            CallMCSL2Dialog(Tip, 0)
-            ServerFolderPath = "./Servers/" + ServerName
-            mkdir(ServerFolderPath)
-            copy(CorePath, ServerFolderPath)
-            ServerConfigDict = {
-                "name": ServerName,
-                "java_path": JavaPath,
-                "min_memory": MinMemory,
-                "max_memory": MaxMemory,
-            }
-            ServerConfigJson = dumps(ServerConfigDict, ensure_ascii=False)
-            with open(r'MCSL2/MCSL2_ServerList.json', "r", encoding='utf-8') as ReadGlobalServerListFile:
-                GlobalServerList = loads(ReadGlobalServerListFile.read())
-                print(GlobalServerList)
-                print(type(GlobalServerList))
-                ServerCount = len(GlobalServerList)
-                print(ServerCount)
-                GlobalServerList['MCSLServerList'].append({
-                    "name": ServerName,
-                    "core_file_name": CoreFileName,
-                    "java_path": JavaPath,
-                    "min_memory": MinMemory,
-                    "max_memory": MaxMemory,
-                })
-                ReadGlobalServerListFile.close()
-            with open(r'MCSL2/MCSL2_ServerList.json', "w", encoding='utf-8') as WriteGlobalServerListFile:
-                WriteGlobalServerListFile.write(dumps(GlobalServerList))
-                WriteGlobalServerListFile.close()
-
-            print(ServerConfigJson)
-            ConfigPath = "Servers//" + ServerName + "//" + "MCSL2ServerConfig.json"
-            with open(ConfigPath, "w+") as SaveConfig:
-                SaveConfig.write(ServerConfigJson)
-                SaveConfig.close()
-            SaveConfig = open(ConfigPath, "w+")
-            SaveConfig.write(ServerConfigJson)
-            SaveConfig.close()
-            Tip = "服务器部署完毕！"
+            ServerSaver.SaveServer(Tip, ServerName, CorePath, JavaPath, MinMemory, MaxMemory, CoreFileName)
             MinMemStatus = 0
             MaxMemStatus = 0
             NameStatus = 0
             JavaStatus = 0
             CoreStatus = 0
             JavaPath = 0
-            CallMCSL2Dialog(Tip, 0)
         else:
             Tip = "服务器部署失败，\n\n但不是你的问题，\n\n去找开发者反馈吧！"
-            CallMCSL2Dialog(Tip, 0)
+            CallMCSL2Dialog(Tip, isNeededTwoButtons=0)
 
     def AutoDetectJava(self):
         # 防止同时多次运行worker线程
@@ -867,23 +704,15 @@ class MCSL2MainWindow(QMainWindow, Ui_MCSL2_MainWindow):
 
     # The function of checking update
     def CheckUpdate(self):
-        CheckUpdateUrlPrefix = "http://api.2018k.cn/checkVersion?id=BCF5D58B4AE6471E98CFD5A56604560B&version="
-        CheckUpdateUrl = CheckUpdateUrlPrefix + Version
-        LatestVersionInformation = get(CheckUpdateUrl).text.split("|")
-        if LatestVersionInformation[0] == "true":
-            # New version.
-            UpdateDownloadUrl = LatestVersionInformation[3]
-            GetUpdateContentsUrl = "http://api.2018k.cn/getExample?id=BCF5D58B4AE6471E98CFD5A56604560B&data=remark"
-            WhatInLatestVersion = get(GetUpdateContentsUrl).text
-            LatestVersionNumber = LatestVersionInformation[4]
-            self.Update_Introduction_Title_Label.setText(
-                "这是最新版本" + LatestVersionNumber + "的说明："
-            )
-            self.Update_Introduction_Label.setText(WhatInLatestVersion)
+        LatestVersionInformation = Updater(Version).GetLatestVersionInformation()
+        if LatestVersionInformation[0] == 1:
+            LatestVersionInformation = str(LatestVersionInformation[1]).replace("(", "").replace(")", "").replace(" ", "").replace("\'", "").split(",")
+            self.Update_Introduction_Title_Label.setText("这是最新版本" + LatestVersionInformation[0] + "的说明：")
+            self.Update_Introduction_Label.setText(str(LatestVersionInformation[1]).replace("\\n", "\n"))
             self.FunctionsStackedWidget.setCurrentIndex(8)
-        elif LatestVersionInformation[0] == "false":
-            Tip = "已经是最新版！"
-            CallMCSL2Dialog(Tip, isNeededTwoButtons=0)
+        else:
+            pass
+
 
 
 if __name__ == '__main__':
@@ -937,6 +766,7 @@ if __name__ == '__main__':
                      "}"
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+    environ["QT_QPA_PLATFORM"] = "wayland"
     environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "auto"
     MCSLProcess = QApplication(argv)
     MCSLMainWindow = MCSL2MainWindow()
