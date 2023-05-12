9f8db274fe7db4bb9894de9ce738e158094a1565
LxHTT <lxhtz.dl@qq.com>
2023-05-12 21:51:56 +0800

    添加了需要联网的代码的容错机制，防止因网络错误/无网络导致的程序崩溃

--- MCSL2_Libs/MCSL2_Updater.py
+++ MCSL2_Libs/MCSL2_Updater.py
@@ -7,25 +7,38 @@ class Updater:
         self.CheckUpdateUrl = self.CheckUpdateUrlPrefix + Version
 
     def GetLatestVersionInformation(self):
-        LatestVersionInformation = get(self.CheckUpdateUrl).text.split("|")
+        try:
+            LatestVersionInformation = get(self.CheckUpdateUrl).text.split("|")
+        except:
+            LatestVersionInformation = ["failed", "failed", "failed", "failed", "failed"]
         if LatestVersionInformation[0] == "true":
             Arg = self.GetMoreInformation(LatestVersionInformation)
             return 1, Arg
         elif LatestVersionInformation[0] == "false":
-            CallMCSL2Dialog("ProgramVersionIsUpToDate",OtherTextArg=None, isNeededTwoButtons=0, ButtonArg=None)
-            return [0]
+            CallMCSL2Dialog("ProgramVersionIsUpToDate", OtherTextArg=None, isNeededTwoButtons=0, ButtonArg=None)
+            Arg = self.GetMoreInformation(LatestVersionInformation)
+            return 0, Arg
+        elif LatestVersionInformation[0] == "failed":
+            CallMCSL2Dialog("ProgramCheckUpdateFailed", OtherTextArg=None, isNeededTwoButtons=0, ButtonArg=None)
+            return -1, ""
         else:
             pass
 
     def GetMoreInformation(self, LatestVersionInformation):
         UpdateDownloadUrl = LatestVersionInformation[3]
         GetUpdateContentsUrl = "http://api.2018k.cn/getExample?id=BCF5D58B4AE6471E98CFD5A56604560B&data=remark"
-        WhatInLatestVersion = get(GetUpdateContentsUrl).text
-        LatestVersionNumber = LatestVersionInformation[4]
-        return LatestVersionNumber, WhatInLatestVersion
+        try:
+            WhatInLatestVersion = get(GetUpdateContentsUrl).text
+            LatestVersionNumber = LatestVersionInformation[4]
+            return LatestVersionNumber, WhatInLatestVersion
+        except:
+            return
 
     def GetNoticeText(self):
         GetNoticeUrl = "http://api.2018k.cn/getExample?id=BCF5D58B4AE6471E98CFD5A56604560B&data=notice"
         GetTodayUserCountUrl = "http://api.2018k.cn/today?id=BCF5D58B4AE6471E98CFD5A56604560B"
-        Notice = get(GetNoticeUrl).text
-        return Notice
+        try:
+            Notice = get(GetNoticeUrl).text
+            return Notice
+        except:
+            return "网络连接失败，无法获取公告。"
