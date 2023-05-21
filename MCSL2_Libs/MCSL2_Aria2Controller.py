f154e1ce2209012191037e42e3f3f5dadf9ff3d3
LxHTT <lxhtz.dl@qq.com>
2023-05-21 15:43:12 +0800

    aria2控制器（未完成）

--- MCSL2_Libs/MCSL2_Aria2Controller.py
+++ MCSL2_Libs/MCSL2_Aria2Controller.py
@@ -1,3 +1,49 @@
+from shutil import which
+from subprocess import CalledProcessError, check_output, Popen
+
+from aria2p import Client, API
+from platform import system
+from os import path as ospath
+
+MCSL2_Aria2Client = API(Client(host="http://localhost", port=6800, secret=""))
+
+
+def LinuxCheckPackageExists(PackageName):
+    try:
+        check_output(["which", PackageName])
+        return True
+    except CalledProcessError:
+        return False
+
+
+
+def LinuxInstallAria2():
+    if which("apt"):
+        cmd = ["apt", "install", "-y", "aria2"]
+    elif which("pacman"):
+        cmd = ["pacman", "-Sy", "--noconfirm", "aria2"]
+    elif which("yum"):
+        cmd = ["yum", "install", "-y", "aria2"]
+    else:
+        return "No"
+    try:
+        Popen(cmd, check=True)
+    except CalledProcessError:
+        return "InstallFailed"
+
+    return "Installed"
+
+
+def CheckMCSL2Aria2IsExist():
+    CurrentSystem = system().lower()
+    if 'windows' in CurrentSystem:
+        if not ospath.exists(r"MCSL2/Aria2/aria2c.exe"):
+            return 1
+        else:
+            return 0
+    elif 'Linux' in CurrentSystem:
+        LinuxCheckPackageExists('aria2')
+
+
 class Aria2Controller():
     pass
-# MCSL2_Aria2Client = Client(host="http://localhost", port=60800, secret="")
