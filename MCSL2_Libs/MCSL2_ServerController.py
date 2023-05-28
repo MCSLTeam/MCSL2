4db18b9e73c8058e6974a9815827fd61b95d0840
AresConnor <2837434884@qq.com>
2023-05-28 12:53:34 +0800

    1.为Aria2Controller添加了一些逻辑逻辑,让他看起来更像是一个controller(bushi),添加了DownloadWatcher线程，用于每隔一段时间记录并发送aria2的下载情况(针对单个下载任务) 2.完成了服务器的交互接口(ServerHandler类),包括日志的更新等,具体功能见代码

--- MCSL2_Libs/MCSL2_ServerController.py
+++ MCSL2_Libs/MCSL2_ServerController.py
@@ -3,8 +3,9 @@ from os import mkdir
 from os.path import realpath
 from shutil import copy
 from subprocess import Popen, PIPE
+from typing import List, Optional
 
-from PyQt5.QtCore import QProcess
+from PyQt5.QtCore import QProcess, QObject, pyqtSignal
 
 from MCSL2_Libs.MCSL2_Dialog import CallMCSL2Dialog
 from MCSL2_Libs.MCSL2_Settings import MCSL2Settings
@@ -219,6 +220,95 @@ def ReadGlobalServerConfig():
         ServerCount = len(GlobalServerList)
         ReadGlobalServerConfigFile.close()
     return ServerCount, GlobalServerList
+
+
+class Server:
+    def __init__(self):
+        self.Process: Optional[QProcess] = None
+        self.LastOutputSize = 0
+
+
+class ServerHandler(QObject):
+    # 当服务器输出日志时发出的信号(发送一个字符串)
+    ServerLogOutput = pyqtSignal(str)
+
+    # 当服务器关闭时发出的信号(发送一个整数exit code)
+    ServerClosed = pyqtSignal(int)
+
+    # 当服务器重启时发出的信号
+    ServerRestarted = pyqtSignal()
+
+    def __init__(self, JavaPath: str, Args: List[str]):
+        """
+        初始化一个服务器处理器
+        """
+        super().__init__()
+        self.JavaPath = JavaPath
+        self.Args = Args
+        self.Server = self.GetServerProcess()
+
+    def GetServerProcess(self) -> Server:
+        """
+        获取一个服务器进程，但是并没有运行，只是创建了一个QProcess对象
+        """
+        server = Server()
+        server.Process = QProcess()
+        server.Process.setProgram(self.JavaPath)
+        server.Process.setArguments(self.Args)
+
+        self.Server.Process.readyReadStandardOutput.connect(self.ServerLogOutputHandler())
+        self.Server.Process.finished.connect(lambda: self.ServerClosed.emit(self.Server.Process.exitCode()))
+
+        return server
+
+    def ServerLogOutputHandler(self):
+        """
+        当服务器输出有变化时，发射信号，发送新的输出(更新的部分)
+        """
+        NewData = self.Server.Process.readAllStandardOutput()
+        DataSize = NewData.size()
+        if DataSize > self.Server.LastOutputSize:
+            # 截取新的输出,由于data可能会达到MB级别，所以使用memoryview来对付QByteArray
+            NewOutput = memoryview(NewData)[self.Server.LastOutputSize:DataSize].tobytes().decode("utf-8")
+            self.ServerLogOutput.emit(NewOutput)
+            self.Server.LastOutputSize = DataSize
+
+    def StartServer(self):
+        """
+        运行服务器
+        """
+        self.Server.Process.start()
+
+    def StopServer(self):
+        """
+        停止服务器
+        """
+        self.Server.Process.write(b"stop\n")
+
+    def RestartServer(self):
+        """
+        重启服务器
+        """
+        self.Server.Process.write(b"stop\n")
+        self.Server.Process.waitForFinished()
+        self.Server.Process.start()
+        self.ServerRestarted.emit()
+
+    def HaltServer(self):
+        """
+        强制停止服务器
+        """
+        self.Server.Process.kill()
+        self.Server.Process.waitForFinished()
+
+    def SendCommand(self, Command: str):
+        """
+        用户向服务器发送命令
+        """
+        self.Server.Process.write(f"{Command}\n".encode("utf-8"))
+
+    def IsServerRunning(self):
+        return self.Server.Process.state() == QProcess.Running
 
 
 class ServerLauncher:
