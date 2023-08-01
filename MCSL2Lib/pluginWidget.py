4937212167111d89b6bb1982d370389598be5644
LxHTT <lxhtz.dl@qq.com>
2023-08-01 14:42:09 +0800

    单个插件CardWidget

---
+++ MCSL2Lib/pluginWidget.py
@@ -0,0 +1,100 @@
+from PyQt5.QtWidgets import (
+    QWidget,
+    QSizePolicy,
+    QVBoxLayout,
+    QHBoxLayout,
+    QSpacerItem
+)
+from PyQt5.QtCore import QSize
+
+from qfluentwidgets import (
+    BodyLabel,
+    CardWidget,
+    PixmapLabel,
+    StrongBodyLabel,
+    SwitchButton,
+    TransparentToolButton
+)
+
+
+class singlePluginInterface(CardWidget):
+    def __init__(self):
+
+        super().__init__()
+
+        self.setObjectName("singlePluginInterface")
+
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(sizePolicy)
+        self.setMinimumSize(QSize(0, 100))
+        self.setMaximumSize(QSize(16777215, 100))
+        self.horizontalLayout = QHBoxLayout(self)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        spacerItem = QSpacerItem(10, 20, QSizePolicy.Fixed, QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.pluginIcon = PixmapLabel(self)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pluginIcon.sizePolicy().hasHeightForWidth())
+        self.pluginIcon.setSizePolicy(sizePolicy)
+        self.pluginIcon.setMinimumSize(QSize(50, 50))
+        self.pluginIcon.setMaximumSize(QSize(50, 50))
+        self.pluginIcon.setObjectName("pluginIcon")
+
+        self.horizontalLayout.addWidget(self.pluginIcon)
+        self.pluginInfoWidget = QWidget(self)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pluginInfoWidget.sizePolicy().hasHeightForWidth())
+        self.pluginInfoWidget.setSizePolicy(sizePolicy)
+        self.pluginInfoWidget.setMinimumSize(QSize(0, 75))
+        self.pluginInfoWidget.setMaximumSize(QSize(16777215, 75))
+        self.pluginInfoWidget.setObjectName("pluginInfoWidget")
+
+        self.verticalLayout = QVBoxLayout(self.pluginInfoWidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+
+        self.pluginName = StrongBodyLabel(self.pluginInfoWidget)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pluginName.sizePolicy().hasHeightForWidth())
+        self.pluginName.setSizePolicy(sizePolicy)
+        self.pluginName.setObjectName("pluginName")
+
+        self.verticalLayout.addWidget(self.pluginName)
+        self.pluginMoreInfo = BodyLabel(self.pluginInfoWidget)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pluginMoreInfo.sizePolicy().hasHeightForWidth())
+        self.pluginMoreInfo.setSizePolicy(sizePolicy)
+        self.pluginMoreInfo.setObjectName("pluginMoreInfo")
+
+        self.verticalLayout.addWidget(self.pluginMoreInfo)
+        self.horizontalLayout.addWidget(self.pluginInfoWidget)
+        self.SwitchButton = SwitchButton(self)
+        self.SwitchButton.setChecked(False)
+        self.SwitchButton.setObjectName("SwitchButton")
+
+        self.horizontalLayout.addWidget(self.SwitchButton)
+        self.shareButton = TransparentToolButton(self)
+        self.shareButton.setObjectName("shareButton")
+
+        self.horizontalLayout.addWidget(self.shareButton)
+        self.deleteIcon = TransparentToolButton(self)
+        self.deleteIcon.setObjectName("deleteIcon")
+
+        self.horizontalLayout.addWidget(self.deleteIcon)
+
+        # self.pluginName.setText("[插件名称]")
+        # self.pluginMoreInfo.setText("[插件详细信息]")
+        self.SwitchButton.setText("已禁用")
+        self.SwitchButton.setOnText("已启用")
+        self.SwitchButton.setOffText("已禁用")
