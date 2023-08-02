21380bf18d0fcec5d9b1a4dad75054e550d2145a
LxHTT <lxhtz.dl@qq.com>
2023-08-02 23:09:39 +0800

    插件示例

---
+++ PluginExample/PluginExample.py
@@ -0,0 +1,37 @@
+#     Copyright 2023, MCSL Team, mailto:lxhtz.dl@qq.com
+#
+#     Part of "MCSL2", a simple and multifunctional Minecraft server launcher.
+#
+#     Licensed under the GNU General Public License, Version 3.0, with our
+#     additional agreements. (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#        https://github.com/MCSLTeam/MCSL2/raw/master/LICENSE
+#
+################################################################################
+'''
+An example for MCSL2's plugin.
+'''
+
+# 提示：声明Plugin()时，声明的变量名称需与文件夹名、config.json的plugin_name键的值相同！
+
+#实现一个Plugin类
+from Adapters.Plugin import Plugin
+
+PluginExample = Plugin() 
+def load():
+    """写你的代码"""
+    print("load")
+#注册加载代码
+PluginExample.register_loadFunc(load)
+def enable():
+    """写你的代码"""
+    print("enable")
+#注册应用代码
+PluginExample.register_enableFunc(enable)
+def disable():
+    """写你的代码"""
+    print("disable")
+#注册应用代码
+PluginExample.register_disableFunc(disable)
