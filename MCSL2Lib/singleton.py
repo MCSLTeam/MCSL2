f2e01357091844e393374af3314dcc59e3b4397b
LxHTT <lxhtz.dl@qq.com>
2023-08-07 22:40:32 +0800

    S | Copyright

--- MCSL2Lib/singleton.py
+++ MCSL2Lib/singleton.py
@@ -1,3 +1,18 @@
+#     Copyright 2023, MCSL Team, mailto:lxhtz.dl@qq.com
+#
+#     Part of "MCSL2", a simple and multifunctional Minecraft server launcher.
+#
+#     Licensed under the GNU General Public License, Version 3.0, with our
+#     additional agreements. (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#        https://github.com/MCSLTeam/MCSL2/raw/master/LICENSE
+#
+################################################################################
+"""
+The singleton decorator.
+"""
 
 def Singleton(cls):
     """单例化装饰器"""
