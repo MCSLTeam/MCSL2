5b0ab2e6db31655d77a3c7dec4b299a3f516c212
LxHTT <lxhtz.dl@qq.com>
2023-08-29 11:43:28 +0800

    F | 修复右键菜单报错问题

--- MCSL2Lib/selectJavaPage.py
+++ MCSL2Lib/selectJavaPage.py
@@ -138,6 +138,13 @@ class SelectJavaPage(QWidget):
             self.tmpSingleJavaWidget.javaPath.setText(str(JavaPath[i].path))
             self.tmpSingleJavaWidget.javaVer.setText(str(JavaPath[i].version))
             self.tmpSingleJavaWidget.javaVer.setReadOnly(True)
+            self.tmpSingleJavaWidget.javaVer.setReadOnly(True)
+            self.tmpSingleJavaWidget.javaVer.setReadOnly(True)
+            self.tmpSingleJavaWidget.javaVer.setReadOnly(True)
+            self.tmpSingleJavaWidget.javaVer.setReadOnly(True)
+            self.tmpSingleJavaWidget.javaVer.setReadOnly(True)
+            self.tmpSingleJavaWidget.javaVer.setReadOnly(True)
+            self.tmpSingleJavaWidget.javaVer.setReadOnly(True)
             self.javaItemVerticalLayout.addWidget(self.tmpSingleJavaWidget)
 
     def scrollAreaProcessor(self, JavaPath):
