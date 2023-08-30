167066e95e82973dddd2fd9134bff58fc1114b85
LxHTT <lxhtz.dl@qq.com>
2023-08-30 14:26:08 +0800

    O | 优化小工具♂

--- Tools/ParseOneDriveLink/ParseOneDriveLink.py
+++ Tools/ParseOneDriveLink/ParseOneDriveLink.py
@@ -1,11 +1,8 @@
 from pyperclip import copy
 def ParseUrl():
     Url = input().split("/")
-    ShareIdSplitter = Url[7].split("?")
-    ShareId = ShareIdSplitter[0]
-    NewUrl = "https://" + Url[2] + "/" + Url[5] + "/" + Url[6] + "/_layouts/52/download.aspx?share=" + ShareId
-    print(NewUrl)
-    print("--------------------------------------------------------------------------------")
+    NewUrl = f"https://{Url[2]}/{Url[5]}/{Url[6]}/_layouts/52/download.aspx?share={Url[7].split('?')[0]}"
+    print(f"结果：\n{NewUrl}\n--------------------------------------------------------------------------------")
     copy(NewUrl)
 
 
