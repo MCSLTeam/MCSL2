cc6e8b446bcf243525ae9f0a364377efe5275cd0
AresConnor <39639716+AresConnor@users.noreply.github.com>
2023-10-07 22:03:43 +0800

    加载MCSL2时设置全局线程池大小

--- MCSL2Lib/utils.py
+++ MCSL2Lib/utils.py
@@ -27,7 +27,8 @@ from types import TracebackType
 from typing import Type, Optional, Iterable, Callable, Dict, List
 
 import aria2p
-from PyQt5.QtCore import QUrl, QThread
+import psutil
+from PyQt5.QtCore import QUrl, QThread, QThreadPool
 from PyQt5.QtGui import QDesktopServices
 from darkdetect import theme as currentTheme
 
@@ -110,6 +111,11 @@ def initializeMCSL2():
             serverListTemplate = '{\n  "MCSLServerList": [\n\n  ]\n}'
             serverList.write(serverListTemplate)
     configurationCompleter()
+
+    # set global thread pool
+    QThreadPool.globalInstance().setMaxThreadCount(
+        psutil.cpu_count(logical=True)
+    )
 
 
 def configurationCompleter():
