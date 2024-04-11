9ca5ed83483621f6ac171de1598316ad8b336db1
AresConnor <39639716+AresConnor@users.noreply.github.com>
2024-04-11 11:11:03 +0800

    add FunctionalInterface and clean code

--- MCSL2Lib/ServerControllers/forge/installer/actions/progress_callback.py
+++ MCSL2Lib/ServerControllers/forge/installer/actions/progress_callback.py
@@ -1,2 +1,22 @@
-class ProgressCallback:
-    ...
+import abc
+
+from ..java2python import FunctionalInterface
+
+
+@FunctionalInterface
+class ProgressCallback(metaclass=abc.ABCMeta):
+    def start(self, label: str):
+        self.message(label)
+
+    def stage(self, message: str):
+        self.message(message)
+
+    @abc.abstractmethod
+    def message(self, message: str):
+        ...
+
+    def progress(self, progress: float):
+        pass
+
+
+TO_SDT_OUT = ProgressCallback.of(lambda self, message: print(message))
--- MCSL2Lib/ServerControllers/forge/installer/java2python.py
+++ MCSL2Lib/ServerControllers/forge/installer/java2python.py
@@ -1,25 +1,48 @@
 import abc
 import typing
-from functools import partial
 
 T = typing.TypeVar('T')
 
 
+def FunctionalInterface(cls):
+    if cls.__class__ is not abc.ABCMeta:
+        raise TypeError("@FunctionalInterface can only be applied to abstract classes")
+
+    abstract_methods = {name for name, val in cls.__dict__.items() if getattr(val, "__isabstractmethod__", False)}
+    if len(abstract_methods) != 1:
+        raise TypeError("@FunctionalInterface can only be applied to classes with exactly one abstract method")
+
+    abstract_method_name = abstract_methods.pop()
+
+    def of(func: typing.Callable[..., T]) -> cls:
+        if not callable(func):
+            raise TypeError("@FunctionalInterface: of method must be callable")
+
+        new_cls_dict = cls.__dict__.copy()
+        new_cls_dict[abstract_method_name] = func
+        return type(cls.__name__, (cls,), new_cls_dict)()
+
+    setattr(cls, 'of', of)
+
+    return cls
+
+
+@FunctionalInterface
 class Supplier(typing.Generic[T], metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def get(self) -> T:
         ...
 
-    @classmethod
-    def of(cls, supplier: typing.Callable[[], T]) -> 'Supplier[T]':
-        return _SupplierInstance(supplier)
-
-
-class _SupplierInstance(Supplier[T]):
-
-    def __init__(self, supplier: typing.Callable[[], T]):
-        self.__supplier = partial(supplier)
-
-    def get(self) -> T:
-        return self.__supplier()
+#     @classmethod
+#     def of(cls, supplier: typing.Callable[[], T]) -> 'Supplier[T]':
+#         return _SupplierInstance(supplier)
+#
+#
+# class _SupplierInstance(Supplier[T]):
+#
+#     def __init__(self, supplier: typing.Callable[[], T]):
+#         self.__supplier = partial(supplier)
+#
+#     def get(self) -> T:
+#         return self.__supplier()
